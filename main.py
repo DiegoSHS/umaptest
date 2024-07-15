925e4c6bb96b415b25d7b83c780e0a13ee68c43c
Diego López Ariza <diegoawesome2017@outlook.com>
2024-07-15 02:51:42 -0600

    Updated main.py

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ from umap import umap_
 
 def main():
     """This function runs UMAP on a random dataset and plots the results."""
+    menssage = """
+    Este es un ejemplo de un plot de UMAP
+    creado para una expo de muestra, la base de código
+    es propiedad de CADELmx
+    """
     st.header("UMAP plot")
-    st.write("Este es un ejemplo de un plot de UMAP creado para una expo de muestra, la base de codigo es propiedad de CADELmx")
+    st.write(menssage)
     data = numpy.random.rand(100, 10)
     reducer = umap_.UMAP()
     embedding = reducer.fit_transform(data)
@@ -17,5 +22,14 @@ def main():
         ax.set_title("UMAP plot")
         ax.scatter(embedding[:, 0], embedding[:, 1])
         st.pyplot(fig)
+    st.write("Codigo de esta webapp:")
+    st.code(to_string())
         
+        
+def to_string():
+    """This function returns the code of the main function as a string."""
+    with open("main.py", "r") as file:
+        return file.read()
+
+
 main()
